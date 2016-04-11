897eee76ba017eed08f448291b1d84548ab60777
NowNewStart <j.gelmar@hotmail.de>
2016-04-11 16:55:06 +0200

    Added first layout files and auth

---
+++ resources/views/authed/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-md-offset-1">
+            <h1>Current events</h1>
+            <hr>
+            <h1><small>none :(</small></h1>    
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,27 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layouts.app')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-md-offset-1">
+                <h1>Pick</h1> 
+                <h3>an online PickEm alternative for non-major events</h3>
+                <hr>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+                <div class="row">
+                    <div class="col-lg-5 well">
+                        <h2>What is this?</h2>
+                        <p>Pick is an open-source website to imitate the PickEm Game from the game Counter-Strike: Global Offensive. This website is a kind of addition because we think PickEm is a funny game and want to extend it to non-major events.</p>
+                    </div>
+                    <div class="col-lg-5 col-lg-offset-2 well">
+                        <h2>How does this work?</h2>
+                        <p>You just need to register on the website to play the game. You can pick on games we created before and earn points and be the best picker. There are no prizes though, this is entirely out of fun. There will be a ranking for each event and a monthly ranking.</p>
+                    </div>
+                </div>
+                <hr>
+                <h1>Current Events</h1>
+                <h1><small>none :(</small></h1>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+@endsection
